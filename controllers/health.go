428dda6c7aafcd14502572eb33ca51d58d2334ea
bhoopendrau <bhoopendrau@google.com>
2023-09-16 22:37:56 +0530

    initial commit

---
+++ controllers/health.go
@@ -0,0 +1,12 @@
+package backend
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type HealthController struct{}
+
+func (h HealthController) Status(c *gin.Context) {
+	c.String(http.StatusOK, "Working!")
+}
